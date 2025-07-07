ecabbe6aef9824e53bb3a72889d4b1a4bf5fcf67
Niranjan Kumar <niranjan@example.com>
2025-07-07 14:12:54 +0530

    Update: update new site

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Another site hai ye
+app.use(express.static('public')); 
+app.use('/shop', express.static(__dirname + '/shop'));
 
 
 // Home page
